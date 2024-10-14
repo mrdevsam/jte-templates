11b659745131d8b80d0fa512e1d257eeed765dc4
mrdevsam <mdshoykotislam05@gmail.com>
2024-10-14 19:16:22 +0600

    accessing fragmented elements with the jte template.

--- src/main/java/com/example/jte_templates/JteTemplatesApplication.java
+++ src/main/java/com/example/jte_templates/JteTemplatesApplication.java
@@ -3,6 +3,7 @@ package com.example.jte_templates;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,11 +17,21 @@ public class JteTemplatesApplication {
 }
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/v1/")
 class IndexController{
 
 	@GetMapping("index")
 	public String showIndex() {
 		return "pages/index";
+	}
+}
+
+@Controller
+class TemplateController{
+
+	@GetMapping("/")
+	public String home(Model model) {
+		model.addAttribute("username", "jon-doe");
+		return "pages/home";
 	}
 }
