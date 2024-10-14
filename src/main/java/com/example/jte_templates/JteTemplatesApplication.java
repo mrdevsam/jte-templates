c7eae4db7c35fd7bd684858ce76b65970b503905
mrdevsam <mdshoykotislam05@gmail.com>
2024-10-14 16:05:18 +0600

    staring jte implementations

--- src/main/java/com/example/jte_templates/JteTemplatesApplication.java
+++ src/main/java/com/example/jte_templates/JteTemplatesApplication.java
@@ -2,6 +2,9 @@ package com.example.jte_templates;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class JteTemplatesApplication {
@@ -10,4 +13,14 @@ public class JteTemplatesApplication {
 		SpringApplication.run(JteTemplatesApplication.class, args);
 	}
 
+}
+
+@Controller
+@RequestMapping("/")
+class IndexController{
+
+	@GetMapping("index")
+	public String showIndex() {
+		return "pages/index";
+	}
 }
