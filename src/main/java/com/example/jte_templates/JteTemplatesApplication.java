f785cfc1422d29c9b681632f1c682786d35b4f7a
mrdevsam <mdshoykotislam05@gmail.com>
2024-10-14 14:27:14 +0600

    initial commit

---
+++ src/main/java/com/example/jte_templates/JteTemplatesApplication.java
@@ -0,0 +1,13 @@
+package com.example.jte_templates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JteTemplatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JteTemplatesApplication.class, args);
+	}
+
+}
